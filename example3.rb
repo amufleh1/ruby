741648b500d095e49cf7f5f9a45f01ee639379cc
Am Mu <fr300roadster@gmail.com>
2017-01-08 16:04:17 -0500

    First example

---
+++ example3.rb
@@ -0,0 +1,12 @@
+#example 3 - list of cars and their years
+weekly_report = {john: 2,
+                  ameer:6,
+                  mike:1,
+                  bob:0}
+puts "December 2016 Week 1"
+puts weekly_report[:john]
+puts weekly_report[:ameer]
+puts weekly_report[:mike]
+puts weekly_report[:bob]
+
+
